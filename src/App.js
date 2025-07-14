cb9e7d124884a302ece47a8ba88e306538af9ee1
Hajung Park <4uwheremi@gmail.com>
2025-07-15 04:19:59 +0900

    first edit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <Bs0Square /> 
+          <Bs0Square /> 12121212121212
         </p>
         <a
           className="App-link"

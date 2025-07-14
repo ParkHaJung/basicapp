5ef6c388f258dc89f1ced803f3e7f9ce3b8d2457
Hajung Park <4uwheremi@gmail.com>
2025-07-15 04:30:21 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <Bs0Square /> 12121212121212
+          <Bs0Square /> 12121212121212sdsds
         </p>
         <a
           className="App-link"
